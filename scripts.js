2bd99c6ae6f8c0dbfe12a587da3fc6efd1779e5f
Onalenna Banda <onabanda@gmail.com>
2025-05-05 18:39:32 +0200

    gherkin syntax

--- scripts.js
+++ scripts.js
@@ -3,7 +3,47 @@ const result = document.querySelector("[data-result]");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  const entries = new FormData(event.target);
-  const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = dividend / divider;
+
+  try {
+    const entries = new FormData(event.target);
+    const { dividend, divider } = Object.fromEntries(entries);
+
+    // Scenario: Validation when values are missing.
+    if (!dividend.trim() || !divider.trim()) {
+      result.innerText =
+        "Division not performed. Both values are required in inputs. Try again";
+      return;
+    }
+
+    // Convert input values to numbers.
+    const dividendNumber = Number(dividend);
+    const dividerNumber = Number(divider);
+
+    // Scenario: Providing anything that is not a number should crash the program.
+    if (isNaN(dividendNumber) || isNaN(dividerNumber)) {
+      throw new Error("Non-numeric input provided");
+    }
+
+    // Scenario: An invalid division (division by zero).
+    if (dividerNumber === 0) {
+      result.innerText =
+        "Division not performed. Invalid number provided. Try again";
+      console.error(new Error("Division by zero error"));
+      return;
+    }
+
+    // Perform division.
+    let divisionResult = dividendNumber / dividerNumber;
+    // Scenario: Dividing numbers result in a decimal number.
+    // Remove the decimal portion so that 20 ÷ 3 becomes 6.
+    divisionResult = Math.trunc(divisionResult);
+
+    result.innerText = divisionResult;
+  } catch (error) {
+    // Log the error with its call stack.
+    console.error(error);
+    // Replace the entire screen with a critical error message.
+    document.body.innerText =
+      "Something critical went wrong. Please reload the page";
+  }
 });
